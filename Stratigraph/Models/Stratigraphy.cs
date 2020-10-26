13348b64b2f7fcc506382f65bc3be3a9b9fcab80
Brendan Abernethy <brendan.j.abernethy@gmail.com>
2020-10-26 14:14:12 -0500

    added models

---
+++ Stratigraph/Models/Stratigraphy.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+/*
+ * [Id] integer PRIMARY KEY IDENTITY,
+  [UserProfileId] integer NOT NULL,
+  [ReportId] integer NOT NULL,
+  [CreateDate] datetime NOT NULL,
+  [Notes] nvarchar(500)
+ */
+namespace Stratigraph.Models
+{
+    public class Stratigraphy
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int UserProfileId { get; set; }
+
+        [Required]
+        public int ReportId { get; set; }
+        
+        [Required]
+        public DateTime CreateDate { get; set; }
+
+        [MaxLength(500)]
+        public string Notes { get; set; }
+    }
+}
---
+++ Stratigraph/Models/UserProfile.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+//Id, UserName, FirstName, LastName, Email, FirebaseId
+
+namespace Stratigraph.Models
+{
+    public class UserProfile
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string UserName { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string LastName { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Email { get; set; }
+
+        [StringLength(28, MinimumLength = 28)]
+        public string FirebaseUserId { get; set; }
+    }
+}
