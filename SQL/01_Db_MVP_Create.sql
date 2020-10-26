a7ac12d5fa21334b42332a09ec2f91dfab61ac5d
Brendan Abernethy <brendan.j.abernethy@gmail.com>
2020-10-26 11:58:02 -0500

    started adding seed data and, modified creat sql file, and setup firebase

--- SQL/01_Db_MVP_Create.sql
+++ SQL/01_Db_MVP_Create.sql
@@ -7,34 +7,38 @@ GO
 USE [StratigraphMVP]
 GO
 
-DROP TABLE IF EXISTS [Stratigraphy];
-DROP TABLE IF EXISTS [Sample];
-DROP TABLE IF EXISTS [Structure];
-DROP TABLE IF EXISTS [User];
-DROP TABLE IF EXISTS [Layer];
+DROP TABLE IF EXISTS [UserProfile];
 DROP TABLE IF EXISTS [Report];
-DROP TABLE IF EXISTS [UserReport];
+DROP TABLE IF EXISTS [Structure];
+DROP TABLE IF EXISTS [Sample];
+DROP TABLE IF EXISTS [Stratigraphy];
+DROP TABLE IF EXISTS [Layer];
+DROP TABLE IF EXISTS [UserProfileReport];
 GO
 
-CREATE TABLE [Stratigraphy] (
+
+CREATE TABLE [UserProfile] (
   [Id] integer PRIMARY KEY IDENTITY,
-  [UserId] integer NOT NULL,
-  [ReportId] integer NOT NULL,
-  [CreateDate] datetime NOT NULL,
-  [Notes] nvarchar(500)
+  [UserName] nvarchar(50) NOT NULL,
+  [FirstName] nvarchar(50) NOT NULL,
+  [LastName] nvarchar(50) NOT NULL,
+  [Email] nvarchar(255) NOT NULL,
+  [FirebaseId] nvarchar(28) NOT NULL
 )
 GO
 
-CREATE TABLE [Sample] (
+CREATE TABLE [Report] (
   [Id] integer PRIMARY KEY IDENTITY,
   [Name] nvarchar(255) NOT NULL,
-  [UserId] integer NOT NULL,
-  [StratigraphyId] integer,
-  [StructureId] integer NOT NULL,
-  [DateTaken] datetime,
-  [Image] nvarchar(255) NOT NULL,
-  [LocationDescription] nvarchar(255),
-  [RoomNumber] integer NOT NULL
+  [CreateDate] datetime NOT NULL,
+  [CompleteDate] datetime
+)
+GO
+
+CREATE TABLE [UserProfileReport] (
+  [Id] integer PRIMARY KEY IDENTITY,
+  [UserProfileId] integer NOT NULL,
+  [ReportId] integer NOT NULL
 )
 GO
 
@@ -48,13 +52,25 @@ CREATE TABLE [Structure] (
 )
 GO
 
-CREATE TABLE [User] (
+CREATE TABLE [Sample] (
   [Id] integer PRIMARY KEY IDENTITY,
-  [UserName] nvarchar(50) NOT NULL,
-  [FirstName] nvarchar(50) NOT NULL,
-  [LastName] nvarchar(50) NOT NULL,
-  [Email] nvarchar(255) NOT NULL,
-  [FirebaseId] nvarchar(28) NOT NULL
+  [Name] nvarchar(255) NOT NULL,
+  [UserProfileId] integer NOT NULL,
+  [StratigraphyId] integer,
+  [StructureId] integer NOT NULL,
+  [DateTaken] datetime,
+  [Image] nvarchar(255) NOT NULL,
+  [LocationDescription] nvarchar(255),
+  [RoomNumber] integer NOT NULL
+)
+GO
+
+CREATE TABLE [Stratigraphy] (
+  [Id] integer PRIMARY KEY IDENTITY,
+  [UserProfileId] integer NOT NULL,
+  [ReportId] integer NOT NULL,
+  [CreateDate] datetime NOT NULL,
+  [Notes] nvarchar(500)
 )
 GO
 
@@ -72,20 +88,6 @@ CREATE TABLE [Layer] (
 )
 GO
 
-CREATE TABLE [Report] (
-  [Id] integer PRIMARY KEY IDENTITY,
-  [Name] nvarchar(255) NOT NULL,
-  [CreateDate] datetime NOT NULL,
-  [CompleteDate] datetime
-)
-GO
-
-CREATE TABLE [UserReport] (
-  [Id] integer PRIMARY KEY IDENTITY,
-  [UserId] integer NOT NULL,
-  [ReportId] integer NOT NULL
-)
-GO
 
 ALTER TABLE [Sample] ADD FOREIGN KEY ([StratigraphyId]) REFERENCES [Stratigraphy] ([Id])
 GO
@@ -93,19 +95,19 @@ GO
 ALTER TABLE [Stratigraphy] ADD FOREIGN KEY ([ReportId]) REFERENCES [Report] ([Id])
 GO
 
-ALTER TABLE [UserReport] ADD FOREIGN KEY ([ReportId]) REFERENCES [Report] ([Id])
+ALTER TABLE [UserProfileReport] ADD FOREIGN KEY ([ReportId]) REFERENCES [Report] ([Id])
 GO
 
-ALTER TABLE [UserReport] ADD FOREIGN KEY ([UserId]) REFERENCES [User] ([Id])
+ALTER TABLE [UserProfileReport] ADD FOREIGN KEY ([UserProfileId]) REFERENCES [UserProfile] ([Id])
 GO
 
 ALTER TABLE [Layer] ADD FOREIGN KEY ([StratigraphyId]) REFERENCES [Stratigraphy] ([Id])
 GO
 
-ALTER TABLE [Sample] ADD FOREIGN KEY ([UserId]) REFERENCES [User] ([Id])
+ALTER TABLE [Sample] ADD FOREIGN KEY ([UserProfileId]) REFERENCES [UserProfile] ([Id])
 GO
 
-ALTER TABLE [Stratigraphy] ADD FOREIGN KEY ([UserId]) REFERENCES [User] ([Id])
+ALTER TABLE [Stratigraphy] ADD FOREIGN KEY ([UserProfileId]) REFERENCES [UserProfile] ([Id])
 GO
 
 ALTER TABLE [Structure] ADD FOREIGN KEY ([ReportId]) REFERENCES [Report] ([Id])
