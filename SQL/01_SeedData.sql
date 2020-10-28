07c5ef8add95266247197869cac0861b91bb5614
Brendan Abernethy <brendan.j.abernethy@gmail.com>
2020-10-28 17:09:49 -0500

    added structure seed data

--- SQL/01_SeedData.sql
+++ SQL/01_SeedData.sql
@@ -32,3 +32,16 @@ insert into UserProfileReport (UserProfileId, ReportId) values (3, 7);
 insert into UserProfileReport (UserProfileId, ReportId) values (2, 8);
 insert into UserProfileReport (UserProfileId, ReportId) values (1, 9);
 insert into UserProfileReport (UserProfileId, ReportId) values (4, 10);
+
+--structure data
+
+insert into Structure (ReportId, Name, Image, Location, YearCunstructed) values (1, 'Rattlesnake, horned', 'http://dummyimage.com/211x222.jpg/ff4444/ffffff', '674 Shopko Street', 1906);
+insert into Structure (ReportId, Name, Image, Location, YearCunstructed) values (2, 'American beaver', 'http://dummyimage.com/151x130.bmp/5fa2dd/ffffff', '6188 Hazelcrest Center', 1804);
+insert into Structure (ReportId, Name, Image, Location, YearCunstructed) values (3, 'Cougar', 'http://dummyimage.com/121x232.bmp/dddddd/000000', '62 Westridge Alley', 1794);
+insert into Structure (ReportId, Name, Image, Location, YearCunstructed) values (4, 'European shelduck', 'http://dummyimage.com/110x164.bmp/cc0000/ffffff', '50 Monica Alley', 1907);
+insert into Structure (ReportId, Name, Image, Location, YearCunstructed) values (5, 'Beisa oryx', 'http://dummyimage.com/145x153.bmp/ff4444/ffffff', '22 Lotheville Crossing', 1904);
+insert into Structure (ReportId, Name, Image, Location, YearCunstructed) values (6, 'Common genet', 'http://dummyimage.com/117x198.bmp/dddddd/000000', '48 Rigney Plaza', 1900);
+insert into Structure (ReportId, Name, Image, Location, YearCunstructed) values (7, 'Sunbird, lesser double-collared', 'http://dummyimage.com/204x122.bmp/cc0000/ffffff', '54 Tennessee Avenue', 1804);
+insert into Structure (ReportId, Name, Image, Location, YearCunstructed) values (8, 'Laughing dove', 'http://dummyimage.com/176x154.bmp/5fa2dd/ffffff', '36356 Sherman Circle', 1891);
+insert into Structure (ReportId, Name, Image, Location, YearCunstructed) values (9, 'Goose, knob-nosed', 'http://dummyimage.com/101x213.png/5fa2dd/ffffff', '898 Cody Court', 1913);
+insert into Structure (ReportId, Name, Image, Location, YearCunstructed) values (10, 'Coot, red-knobbed', 'http://dummyimage.com/216x206.bmp/dddddd/000000', '6598 Novick Plaza', 1889);
