a37617fea74ba0638a2b59aa53206b52b1da14bd
Brendan Abernethy <brendan.j.abernethy@gmail.com>
2021-01-14 11:03:41 -0600

    added finish period model

---
+++ Stratigraph/Models/FinishPeriod.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+/*
+ [Id] integer PRIMARY KEY IDENTITY,
+  [UserProfileId] integer NOT NULL,
+  [StructureId] integer,
+  [Name] nvarchar(255),
+  [StartYear] integer,
+  [EndYear] integer,
+  [OwnerName] nvarchar(255),
+  [Notes] nvarchar(MAX)
+ */
+
+
+namespace Stratigraph.Models
+{
+    public class FinishPeriod
+    {
+        public int Id { get; set; }
+
+        public int UserProfileId { get; set; }
+
+        public int StructureId { get; set; }
+
+        public string Name { get; set; }
+
+        public int StartYear { get; set; }
+
+        public int EndYear { get; set; }
+
+        public string OwnerName { get; set; }
+
+        public string Notes { get; set; }
+
+    }
+}
