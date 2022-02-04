2a264a9f275501f2fc207e9b74a4f6e240a7bad7
Brendan Abernethy <brendan.j.abernethy@gmail.com>
2022-02-04 10:59:37 -0600

    added fallback controller for client

---
+++ Stratigraph/Controllers/FallbackController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Mime;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Stratigraph.Models;
+using Stratigraph.Repositories;
+
+namespace Stratigraph.Controllers
+{
+    public class FallbackController : ControllerBase
+    {
+        public IActionResult Index()
+        {
+            return PhysicalFile(Path.Combine(Directory.GetCurrentDirectory(),
+                "wwwroot", "index.html"), MediaTypeNames.Text.Html);
+        }
+    }
+}
